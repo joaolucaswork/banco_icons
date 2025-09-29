2404652a90145991d6cb7e49987ae75a1f87150c
João Lucas <lucas.26.11.2014@gmail.com>
2025-09-29 05:48:42 -0300

    Add Vercel Speed Insights integration
    Integrated @vercel/speed-insights into the SvelteKit project by updating dependencies and injecting Speed Insights in svelte.config.js. Also updated .gitignore to exclude .vercel directory and adjusted environment variable typings.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
 import adapter from "@sveltejs/adapter-vercel";
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+injectSpeedInsights();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = { kit: { adapter: adapter() } };
